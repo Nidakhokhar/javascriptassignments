a3e4d86b84d52750276d7f68f5823477757ec958
Nidakhokhar <57321033+Nidakhokhar@users.noreply.github.com>
2023-09-30 22:10:21 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -48,9 +48,81 @@
 // Chapter 3 
 // (Variables for numbers)
 // 1. Declare a variable “caseQty”
+// var caseQty
+
 // 2. Assign to the variable caseQty, which has already been declared,
 // the value 144.
+// caseQty = 144;
+
 // 3. Rewrite this statement so the variable can be used in a math
 // operation. var num = "9";
+// var num = 9 ;
+
 // 4. In one statement declare a variable. In a second statement assign
 // it the sum of 2 numbers.
+// var num1 = 2;
+// var result = num + num1 ;
+// console.log(result);
+// 5. What is the value of orderTotal?
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// Try it yourself.
+
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var result = merchTotal + shippingCharge ;
+// console.log(result);
+
+// 6. In the first statement declare a variable and assign it a number. In
+// the second statement, change the value of the variable by adding
+// it together with a number.
+// var merchTotal 
+// var merchTotal = 10;
+// var result = merchTotal + 109 ;
+// console.log(result);
+
+
+// Chapter 4 (Variable names Legal and
+//     Illegal)
+//     1. Correct this statement.
+//     var product cost = 3.45;
+// var productCost =3.45;
+
+//     2. Rewrite this using camelCase.
+//     var Nameofband;
+//  var Nameofband;
+//     3. In a single statement declare a legally-named variable and assign a
+//     number to it.
+// var my_practice = 23;
+//     4. Declare a variable that is a combination of your first and last
+//     names. Use camelCase.
+// var nidaKhokhar ;
+//     5. List the legal and Illegal Variables
+// legal 
+// var myname;
+// var myName;
+// var my_name;
+// var _my_name;
+// var $my_name;
+
+// Illegal Variables
+// var @myname;
+// var 123myName;
+// var my name;
+// var m&name;
+// var my-name;
+// Chapter 5 (Math Expression I)
+// 1. What is the name and symbol of the arithmetic operator that
+// gives you the remainder when one number is divided by another?
+// 2. What is the value of num?
+// var num = 20 % 6;
+// 3. In a single statement, declare the variable largeNum and assign it
+// the result of 1,000 multiplied by 2,000.
+// 4. Assign to a variable the value represented by one variable
+// subtracted from the value represented by another variable
+// 5. Assign to a variable the remainder when one number is divided by
+// another. The variable hasn't been declared beforehand. Make up
+// the variable name.
+// 6. Code an alert that displays the result of a multiplication on 2
+// numbers.
