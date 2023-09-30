262b6eed361cb60f6962d574bebea5d5abb3fafb
Nidakhokhar <57321033+Nidakhokhar@users.noreply.github.com>
2023-09-30 10:24:03 +0500

    Add files via upload
    java assignments

---
+++ app.js
@@ -0,0 +1,56 @@
+// 1. Alert these following (individually):
+// // I. First Name
+// alert("First Name");
+
+
+
+
+// II. Last Name
+// alert("Last Name");
+
+
+// III. Email
+// alert("Email");
+
+
+// IV. Phone Number
+// var phonenumber = prompt("Enter phone number");
+// document.write(phonenumber);
+
+
+// V. Password
+// alert("Email");
+// 2. Correct this statement: alert"You're learning JavaScript!";
+// alert("You're learning JavaScript");
+
+// 3. Code an alert statement displaying any message you like.
+// document.write("Dont know why seen the asssignment");
+
+// Chapter 2 (Variables for string)
+// 1. Declare any variable in the camelCase format.
+// var camelCase
+
+// 2. Declare a variable of your choice without defining it. Then, in a
+// second statement, assign it a string of your choice.
+// var camelCase = 24;
+
+
+// 3. Declare the variable teamName and Alert your Team name.
+// var teamName = "NKK TEAM";
+// console.log(teamName);
+// 4. This statement has already been coded. var bestMan = "Charlie";
+// Assign the variable a new string.
+// var bestMan = "Charlie";
+// bestMan = 5;
+// console.log(bestMan);
+
+
+// Chapter 3 
+// (Variables for numbers)
+// 1. Declare a variable “caseQty”
+// 2. Assign to the variable caseQty, which has already been declared,
+// the value 144.
+// 3. Rewrite this statement so the variable can be used in a math
+// operation. var num = "9";
+// 4. In one statement declare a variable. In a second statement assign
+// it the sum of 2 numbers.
